95749360d7801d35c9e7b791a6fbd11dfb25a5c1
Florian Mahon <florian.mahon@gmail.com>
2019-03-12 22:44:10 +0100

    REF: clean code remove used code

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <hal/hal.h>
 #include <SPI.h>
 #include <U8x8lib.h>
-//#include "config.h"
+#include "config.h"
 #ifndef CONFIG_H_
   #include <example_config.h>
   /**
@@ -32,38 +32,8 @@
 #endif
 
 
-typedef union {
-  float f[2];               // Assigning fVal.f will also populate fVal.bytes;
-  unsigned char bytes[8];   // Both fVal.f and fVal.bytes share the same 4 bytes of memory.
-} floatArr2Val;
-
-floatArr2Val latlong;
-float latitude;
-float longitude;
-char s[16]; 
-
-// the OLED used
-U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
-
-void os_getArtEui (u1_t* buf) {
-  memcpy_P(buf, APPEUI, 8);
-}
-
-void os_getDevEui (u1_t* buf) {
-  memcpy_P(buf, DEVEUI, 8);
-}
-
-void os_getDevKey (u1_t* buf) {
-  memcpy_P(buf, APPKEY, 16);
-}
-void do_send(osjob_t* j);
-static osjob_t sendjob;
-
-// Schedule TX every this many seconds (might become longer due to duty
-// cycle limitations).
 const unsigned TX_INTERVAL = 30;
 
-// Pin mapping
 const lmic_pinmap lmic_pins = {
   .nss = 18,
   .rxtx = LMIC_UNUSED_PIN,
@@ -71,30 +41,23 @@ const lmic_pinmap lmic_pins = {
   .dio = {26, 33, 32},
 };
 
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
 
-void get_coords () {
-  latitude  = 1000;
-  longitude = 2000;
-  // Only update if location is valid and has changed
-  if ((latitude && longitude) && latitude != latlong.f[0]
-      && longitude != latlong.f[1]) {
-    latlong.f[0] = latitude;
-    latlong.f[1] = longitude;
-    for (int i = 0; i < 8; i++)
-      Serial.print(latlong.bytes[i], HEX);
-    Serial.println();
+static osjob_t sendjob;
+
+void send_task(osjob_t* j) {
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+    u8x8.drawString(0, 7, "OP_TXRXPEND, not sent");
+  } else {
+    LMIC_setTxData2(1, (xref2u1_t)"HelloWorld", 11, 0);
+  
+    Serial.println(F("Packet queued"));
+    u8x8.drawString(0, 7, "PACKET QUEUED");
+    digitalWrite(BUILTIN_LED, HIGH);
   }
-  u8x8.setCursor(0, 2);
-  u8x8.print("Lat: ");
-  u8x8.setCursor(5, 2);
-  sprintf(s, "%f", latitude);
-  u8x8.print(s);
-  u8x8.setCursor(0, 3);
-  u8x8.print("Lng: ");
-  u8x8.setCursor(5, 3);
-  sprintf(s, "%f", longitude);
-  u8x8.print(s);
 }
+
 void onEvent (ev_t ev) {
   switch (ev) {
     case EV_SCAN_TIMEOUT:
@@ -156,7 +119,7 @@ void onEvent (ev_t ev) {
         u8x8.printf("RSSI %d SNR %.1d", LMIC.rssi, LMIC.snr);
       }
       // Schedule next transmission
-      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), send_task);
       break;
     case EV_LOST_TSYNC:
       Serial.println(F("EV_LOST_TSYNC"));
@@ -186,22 +149,19 @@ void onEvent (ev_t ev) {
       break;
   }
 }
-void do_send(osjob_t* j) {
-  // Check if there is not a current TX/RX job running
-  if (LMIC.opmode & OP_TXRXPEND) {
-    Serial.println(F("OP_TXRXPEND, not sending"));
-    u8x8.drawString(0, 7, "OP_TXRXPEND, not sent");
-  } else {
-    // Prepare upstream data transmission at the next possible time.
-    get_coords();
-    //LMIC_setTxData2(1, (uint8_t*) coords, sizeof(coords), 0);
-    LMIC_setTxData2(1, latlong.bytes, 8, 0);
-    Serial.println(F("Packet queued"));
-    u8x8.drawString(0, 7, "PACKET QUEUED");
-    digitalWrite(BUILTIN_LED, HIGH);
-  }
-  // Next TX is scheduled after TX_COMPLETE event.
+
+void os_getArtEui (u1_t* buf) {
+  memcpy_P(buf, APPEUI, 8);
 }
+
+void os_getDevEui (u1_t* buf) {
+  memcpy_P(buf, DEVEUI, 8);
+}
+
+void os_getDevKey (u1_t* buf) {
+  memcpy_P(buf, APPKEY, 16);
+}
+
 
 void setup() {
   
@@ -214,14 +174,10 @@ void setup() {
   Serial.begin(9600);
   u8x8.begin();
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.drawString(0, 1, "Remon WebLoRa");
-  //SPI.begin(5, 19, 27);
-  // LMIC init
+  u8x8.drawString(0, 1, "ADNT FirstTest");
   os_init();
-  // Reset the MAC state. Session and pending data transfers will be discarded.
   LMIC_reset();
-  // Start job (sending automatically starts OTAA too)
-  do_send(&sendjob);
+  send_task(&sendjob);
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(BUILTIN_LED, HIGH);
 }
